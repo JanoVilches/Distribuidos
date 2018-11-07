d5dc112c1b02a93a792532a2dfe911dcc4fb8869
abdel <apdelsm@gmail.com>
2018-11-07 00:24:03 -0300

    avance logica avión

--- avion_client.rb
+++ avion_client.rb
@@ -1,13 +1,54 @@
 require 'grpc'
-require_relative 'Aeropuerto_services_pb'
+require './Aeropuerto_services_pb'
 
+def regularPeso(avion)
 
-stub = Torre::Stub.new('[::]:50051', :this_channel_is_insecure)
-aviones = []
-for i in 0..5
-    aviones << Avion.new(aerolinea: "a", codigo_avion: i.to_s, peso: 1, combustible: 1, torre_control_inicial: "as", torre_control_destino: "asd")
 end
-aviones.each do |i|
-    response = stub.aterrizar(i)
-    puts response.altura
+
+class PlaneServer < Plane::Service
+    
+end
+
+puts "Bienvenido al vuelo"
+puts "[Avion] Nombre de la Aerolínea y número de Avion:"
+name = gets.chomp
+aerolinea = name.split[-1]
+name = name.split[0]
+puts "[Avion - #{name}]: Peso Máximo de carga [Kg]:"
+peso = gets.chomp
+peso = peso.to_i
+puts "[Avion - #{name}]: Capacidad del tanque de combustible [L]:"
+estanque = gets.chomp
+estanque = estanque.to_i
+puts "[Avion - #{name}]: Torre de control inicial:"
+destino = gets.chomp
+
+stub = Torre::Stub.new('destino:50051', :this_channel_is_insecure)
+
+avion = Avion.new(aerolinea: aerolinea, codigo_avion: name, peso: peso, combustible: estanque, torre_control_destino: destino, combustibleActual: (estanque*0.5).to_i, pesoActual: (peso*0.8).to_i)
+
+response = stub.aterrizar(avion)
+
+puts "[Avion - #{name}]: Esperando pista de aterrizaje..."
+
+if response.pista != 0
+    puts "[Avion - #{name}]: Aterrizando en la pista #{response.pista}..."
+    puts "[Avion - #{name}]: Presione enter para despegar"
+    puts "[Avion - #{name}]: Ingrese destino"
+    destino = gets.chomp
+    avion.torre_control_destino = destino
+    response = stub.gate(avion)
+    puts "[Avion - #{name}]: Pasando por el Gate..."
+    avion.combustibleActual = rand avion.combustibleActual..avion.combustible
+    avion.pesoActual = rand (avion.peso*0.5).to_i..(avion.peso*1.5).to_i
+    puts "[Avion - #{name}]: Todos los pasajeros a bordo y combustible cargado."
+    puts "[Avion - #{name}]: Pasando por el Gate..."
+    response = stub.despegar(avion)
+    if response.estado == 0
+        #server
+    elsif response.estado == -1
+
+    end    
+else
+
 end
